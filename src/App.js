1727a6cc1b6066e610c87d36462696fc471a10de
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-03-22 10:47:26 -0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Movie from './components/Movie/Movie';
+import Session from './components/Session/Session';
+import Success from './components/Success/Success';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/filme" element={<Movie />} />
+				<Route path="/sessao" element={<Session />} />
+				<Route path="/sucesso" element={<Success />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
