c723bddf114e3c79d5eb4b52f55d8f70c43f01fa
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-03-28 10:33:32 -0300

    add: buyer info input visual distinction between seats

--- src/components/Seats/BuyerInfoInput.jsx
+++ src/components/Seats/BuyerInfoInput.jsx
@@ -1,28 +1,33 @@
 export default function BuyerInfoInput({ id, orderInfo, setOrderInfo }) {
+	const buyer = orderInfo.find((order) => order.seatId === id);
+
+	if (!buyer) return '';
 	return (
-		<label>
-			<p>Nome do comprador</p>
-			<input
-				type="text"
-				onChange={handleInputChange}
-				placeholder="Digite seu nome..."
-				name="name"
-				required
-			></input>
-			<p>CPF do comprador</p>
-			<input
-				type="text"
-				onChange={handleInputChange}
-				placeholder="Digite seu CPF..."
-				name="cpf"
-				required
-			></input>
-		</label>
+		<div className="BuyerInfo-wrapper">
+			<h3>Assento {buyer.seatNumber}</h3>
+			<label>
+				<p>Nome do comprador</p>
+				<input
+					type="text"
+					onChange={handleInputChange}
+					placeholder="Digite seu nome..."
+					name="name"
+					required
+				></input>
+				<p>CPF do comprador</p>
+				<input
+					type="number"
+					onChange={handleInputChange}
+					placeholder="Digite seu CPF..."
+					name="cpf"
+					required
+				></input>
+			</label>
+		</div>
 	);
 
 	function handleInputChange(event) {
 		const nameOrCpfInput = event.target;
-		const buyer = orderInfo.find((order) => order.seatId === id);
 
 		buyer[nameOrCpfInput.name] = nameOrCpfInput.value;
 		setOrderInfo([...orderInfo]);
